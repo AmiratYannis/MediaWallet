7af1cef524a008a7076e66cf9cf8d2a1fce44f60
AmiratYannis <46192264+AmiratYannis@users.noreply.github.com>
2022-10-17 16:32:12 +0200

    Add Videos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,94 @@
-import Header from './Header';
+import React,{useState,useEffect} from 'react';
+import axios from "axios";
+import Video from './Video';
+
 import './App.css';
 
+const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=FR&key=AIzaSyBs_luxs6WTBecj3yCXDe8ECEAiWkvBGIY";
+
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [popularVideos, setPopularVideos] = useState([]);
+  const [searchVideosResults,setSearchVideos] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+    
+
+  useEffect(() => {
+      axios.get(YOUTUBE_API_URL)
+      .then((res)=>setPopularVideos(res.data.items));
+  }, []);
+
+  const handleSearchInputChanges = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const resetInputField = () => {
+    setSearchValue("");
+  }
+
+  const search=(e)=>{
+
+    e.preventDefault();
+   
+   
+    
+    setSearchVideos([]);
+    
+    axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q="+searchValue+"&key=AIzaSyBs_luxs6WTBecj3yCXDe8ECEAiWkvBGIY")
+    .then((res)=>setSearchVideos(res.data.items))
+    
+  }
+
+
   return (
-    <Header/>
+    <div className="mediaWallet">
+      <div id="head"> 		   	  	  				  
+ 		   	  	  				  
+        <h1 id="title" onclick="window.location.reload();">MEDIA WALLET</h1> 
+        <div className="search"> 		   	  	  				  		   	  	  				  
+          <label> Rechercher un contenu </label> 		   	  	  			
+          <input  id="contenu" 
+                  type="text" name="contenu" 
+                  value={searchValue}
+                  onChange={handleSearchInputChanges} /> 		   	  	  				  
+          <button id="btnSearch" onClick={search}>Search</button> 		   	  	  				  
+        </div>    	  	  				  
+                                                 
+      </div>
+
+      <div id="home">
+        <div id="result"> 	   	  	  				  
+            <div id="youtube">
+              {searchVideosResults
+              .map((video,index)=>(
+                <Video key={index} video={video}/>
+              ))
+
+              }
+            </div> 		   	  	  				   	   	  	  				  
+        </div> 	
+
+        <div id="incontournables"> 		   	  	  				  
+                          
+          <h3> Videos populaires du moments </h3> 		   	  	  				  
+          <div class="YouTube"> 		   	  	  				  
+             {popularVideos
+             .map((video,index)=>(
+              <Video key={index} video={video}/>
+             ))
+
+             }                                           
+          </div> 		   	  	  				  
+                            
+        </div> 		
+      </div>
+         	 
+    </div>
+    
+    
+ 
   );
 }
 
---
+++ src/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Video = ({video}) =>{
+    console.log(video);
+    return (
+        <div className="card cardYoutube">
+            <h1>Video</h1>
+        </div>
+    );
+}
+
+export default Video;
